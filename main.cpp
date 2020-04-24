07af5d6a507bcc09abf1b302f7906f470c404d38
thebigsmileXD <thebigsmilexd@gmail.com>
2020-04-24 23:04:41 +0200

    Initial working hello world mod

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <dllentry.h>
 #include <log.h>
 #include "settings.h"
-#include "playerdb.h"
+//#include "playerdb.h"
+#include <command.h>
+#include <Command/Command.h>
+#include <Command/CommandRegistry.h>
+#include <Command/CommandPermissionLevel.h>
+#include <Command/CommandFlag.h>
+#include <Command/CommandParameterData.h>
+#include <Command/CommandOutput.h>
+#include <Command/CommandOrigin.h>
 
-DEF_LOGGER("SampleMod");
+DEF_LOGGER("EZMod1");
 DEFAULT_SETTINGS(settings);
 
 // PS: Reserved for compatibility purposes
@@ -17,10 +25,28 @@ void PreInit() {
   LOGV("pre init");
   // You can use the event system to receive and process events
   // The following is an example
-  Mod::PlayerDatabase::GetInstance().AddListener(SIG("joined"), [](Mod::PlayerEntry const &entry) {
-    LOGV("joined name: %s, xuid: %d") % entry.name % entry.xuid;
-  });
-  Mod::PlayerDatabase::GetInstance().AddListener(
-      SIG("left"), [](Mod::PlayerEntry const &entry) { LOGV("left name: %s, xuid: %d") % entry.name % entry.xuid; });
+//  Mod::PlayerDatabase::GetInstance().AddListener(SIG("joined"), [](Mod::PlayerEntry const &entry) {
+//    LOGV("joined name: %s, xuid: %d") % entry.name % entry.xuid;
+//  });
+//  Mod::PlayerDatabase::GetInstance().AddListener(
+//      SIG("left"), [](Mod::PlayerEntry const &entry) { LOGV("left name: %s, xuid: %d") % entry.name % entry.xuid; });
 }
-void PostInit() { LOGV("post init"); }
+
+class HelloWorldCommand : public Command {
+public:
+  HelloWorldCommand() {}
+
+  void execute(CommandOrigin const &origin, CommandOutput &output) { output.success("Hello world!"); }
+  static void setup(CommandRegistry *registry) {
+    registry->registerCommand(
+        "helloworld", "My first command", CommandPermissionLevel::Any, CommandFlagCheat, CommandFlagNone);
+    registry->registerOverload<HelloWorldCommand>("helloworld");
+  }
+};
+
+void PostInit() {
+  LOGV("post init");
+  Mod::CommandSupport::GetInstance().AddListener(SIG("loaded"), &HelloWorldCommand::setup);
+}
+
+void ServerStart() { LOGV("server start"); }
